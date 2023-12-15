53a081202416a3f458f91b111c0da5bdb9facd26
Charminha <83584290+Charminha@users.noreply.github.com>
2023-12-15 20:36:18 +0100

    Add one row of pixels

--- javascript.js
+++ javascript.js
@@ -1,4 +1,8 @@
 const sketchpad = document.querySelector('.sketchpad');
-const pixel = document.createElement('div');
-pixel.setAttribute('style', 'width: 32px; height: 32px; border: medium solid black; background-color: white;');   
-sketchpad.appendChild(pixel);
+for (i=0; i<16; i++){
+    for (i=0; i<16; i++){
+    const pixel = document.createElement('div');
+    pixel.setAttribute('style', 'width: 32px; height: 32px; border: 1px dotted black; background-color: white; flex-wrap: wrap;');   
+    sketchpad.appendChild(pixel);
+    }
+}
