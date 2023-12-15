c026726bddfa2f35af49c02adcbd15ebb6fb424f
Charminha <83584290+Charminha@users.noreply.github.com>
2023-12-15 20:20:21 +0100

    Add one pixel to sketchpad

---
+++ javascript.js
@@ -0,0 +1,4 @@
+const sketchpad = document.querySelector('.sketchpad');
+const pixel = document.createElement('div');
+pixel.setAttribute('style', 'width: 32px; height: 32px; border: medium solid black; background-color: white;');   
+sketchpad.appendChild(pixel);
