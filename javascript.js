d4ea527a1f43bc9b685151fab1f204b170f563ba
Charminha <83584290+Charminha@users.noreply.github.com>
2023-12-17 21:54:40 +0100

    Fix flex layout of grid

--- javascript.js
+++ javascript.js
@@ -2,10 +2,10 @@ const sketchpad = document.querySelector('.sketchpad');
 
 for (i=0; i<15; i++){
     pixelRow = document.createElement('div');
-    //pixelRow.setAttribute('display-style: flex;');
+    pixelRow.setAttribute('style', 'display: flex; flex-direction: column; flex-grow: 1;');
     for (j=0; j<15; j++){
         const pixel = document.createElement('div');
-        pixel.setAttribute('style', 'width: 32px; height: 32px; border: 1px dotted black; background-color: white; flex-wrap: wrap;');   
+        pixel.setAttribute('style', 'width: 32px; height: 32px; border: 1px dotted black; background-color: white; flex-grow: 1;');   
         pixelRow.appendChild(pixel);
     }
     sketchpad.appendChild(pixelRow);
