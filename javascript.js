1c9d3e058487ba65e7ce35599b51ee6fe35d3e22
Charminha <83584290+Charminha@users.noreply.github.com>
2023-12-17 21:45:32 +0100

    Add 16x16 pixel grid

--- javascript.js
+++ javascript.js
@@ -1,8 +1,12 @@
 const sketchpad = document.querySelector('.sketchpad');
-for (i=0; i<16; i++){
-    for (i=0; i<16; i++){
-    const pixel = document.createElement('div');
-    pixel.setAttribute('style', 'width: 32px; height: 32px; border: 1px dotted black; background-color: white; flex-wrap: wrap;');   
-    sketchpad.appendChild(pixel);
+
+for (i=0; i<15; i++){
+    pixelRow = document.createElement('div');
+    //pixelRow.setAttribute('display-style: flex;');
+    for (j=0; j<15; j++){
+        const pixel = document.createElement('div');
+        pixel.setAttribute('style', 'width: 32px; height: 32px; border: 1px dotted black; background-color: white; flex-wrap: wrap;');   
+        pixelRow.appendChild(pixel);
     }
+    sketchpad.appendChild(pixelRow);
 }
