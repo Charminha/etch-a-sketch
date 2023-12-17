9644e0599e1ca3a20adcd7734678b8fb94fdfd65
Charminha <83584290+Charminha@users.noreply.github.com>
2023-12-17 21:56:10 +0100

    Add comments to loops

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
 const sketchpad = document.querySelector('.sketchpad');
 
+// create rows of pixel grid 
 for (i=0; i<15; i++){
     pixelRow = document.createElement('div');
     pixelRow.setAttribute('style', 'display: flex; flex-direction: column; flex-grow: 1;');
+    // fill each row with 16 pixels
     for (j=0; j<15; j++){
         const pixel = document.createElement('div');
         pixel.setAttribute('style', 'width: 32px; height: 32px; border: 1px dotted black; background-color: white; flex-grow: 1;');   
